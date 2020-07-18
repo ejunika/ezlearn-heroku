43b0080c867f93e107c7444d7759375288d3e7a7
Azaz Akhtar <azaz.akhtar@powerschool.com>
2020-07-19 01:03:02 +0530

    check

--- dist/server.js
+++ dist/server.js
@@ -7,34 +7,39 @@ const express_1 = __importDefault(require("express"));
 const swagger_ui_express_1 = require("swagger-ui-express");
 const path_1 = require("path");
 const yamljs_1 = require("yamljs");
-const mysql_1 = require("mysql");
 const app = express_1.default();
 const PORT = process.env.PORT || 5000;
 const swaggerDocument = yamljs_1.load(path_1.join(__dirname, './api.yaml'));
 app.use('/api-docs', swagger_ui_express_1.serve, swagger_ui_express_1.setup(swaggerDocument));
 app.get('/v1/api/users', (req, res) => {
-    let pool = mysql_1.createPool({
-        host: 'db4free.net',
-        database: 'test_mysql_009',
-        user: 'test_mysql_009',
-        password: 'test_mysql_009',
-        connectionLimit: 1000,
-        connectTimeout: 60 * 60 * 1000,
-        acquireTimeout: 60 * 60 * 1000,
-        timeout: 60 * 60 * 1000,
-    });
-    pool.query('SELECT * FROM app_users', (err, results) => {
-        if (err) {
-            res.json({
-                message: 'Failure',
-                error: err.message
-            });
-        }
-        else {
-            res.json({
-                message: 'Success',
-                data: results
-            });
+    // let pool = createPool({
+    //     host: 'db4free.net',
+    //     database: 'test_mysql_009',
+    //     user: 'test_mysql_009',
+    //     password: 'test_mysql_009',
+    //     connectionLimit: 1000,
+    //     connectTimeout: 60 * 60 * 1000,
+    //     acquireTimeout: 60 * 60 * 1000,
+    //     timeout: 60 * 60 * 1000,
+    // });
+    // pool.query('SELECT * FROM app_users', (err, results) => {
+    //     if (err) {
+    //         res.json({
+    //             message: 'Failure',
+    //             error: err.message
+    //         })
+    //     } else {
+    //         res.json({
+    //             message: 'Success',
+    //             data: results
+    //         });
+    //     }
+    // });
+    res.json({
+        message: 'Success',
+        data: {
+            user_name: 'User Name',
+            password: 'Password'
         }
     });
 });
--- src/server.ts
+++ src/server.ts
@@ -13,27 +13,34 @@ const swaggerDocument = load(join(__dirname, './api.yaml'));
 app.use('/api-docs', serve, setup(swaggerDocument));
 
 app.get('/v1/api/users', (req, res) => {
-    let pool = createPool({
-        host: 'db4free.net',
-        database: 'test_mysql_009',
-        user: 'test_mysql_009',
-        password: 'test_mysql_009',
-        connectionLimit: 1000,
-        connectTimeout: 60 * 60 * 1000,
-        acquireTimeout: 60 * 60 * 1000,
-        timeout: 60 * 60 * 1000,
-    });
-    pool.query('SELECT * FROM app_users', (err, results) => {
-        if (err) {
-            res.json({
-                message: 'Failure',
-                error: err.message
-            })
-        } else {
-            res.json({
-                message: 'Success',
-                data: results
-            });
+    // let pool = createPool({
+    //     host: 'db4free.net',
+    //     database: 'test_mysql_009',
+    //     user: 'test_mysql_009',
+    //     password: 'test_mysql_009',
+    //     connectionLimit: 1000,
+    //     connectTimeout: 60 * 60 * 1000,
+    //     acquireTimeout: 60 * 60 * 1000,
+    //     timeout: 60 * 60 * 1000,
+    // });
+    // pool.query('SELECT * FROM app_users', (err, results) => {
+    //     if (err) {
+    //         res.json({
+    //             message: 'Failure',
+    //             error: err.message
+    //         })
+    //     } else {
+    //         res.json({
+    //             message: 'Success',
+    //             data: results
+    //         });
+    //     }
+    // });
+    res.json({
+        message: 'Success',
+        data: {
+            user_name: 'User Name',
+            password: 'Password'
         }
     });
 });
