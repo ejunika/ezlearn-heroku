94ad3e23a9156f98d88a823ab62bfe34af8cdaeb
Azaz Akhtar <azaz.akhtar@powerschool.com>
2020-07-18 17:29:11 +0530

    Test-2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 
+const mysql = require('mysql');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Success',
-        data: {
-            message: 'Hello world'
+    let pool = mysql.createPool({
+        host: 'db4free.net',
+        database: 'test_mysql_009',
+        user: 'test_mysql_009',
+        password: 'test_mysql_009'
+    });
+    pool.query('SELECT * FROM app_users', (err, results) => {
+        if (err) {
+            throw err;
         }
+        console.log(results);
+        res.json({
+            message: 'Success',
+            data: results
+        });
     });
 });
 
